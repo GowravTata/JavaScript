549c21a8e6bd5727a7e06a4fc449281c5a09d5b7
GowravTata <gowravsaitata@gmail.com>
2023-10-24 16:25:18 +0530

    Array methods, classes, async

---
+++ Working-With-Arrays/async.js
@@ -0,0 +1,10 @@
+'use strict';
+const apiUrl = 'https://countries-api-836d.onrender.com/countries/';
+
+const counties = ['United States of America', 'Portugal'];
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.eu/rest/v2/name/portugal');
+request.send();
+
+request.addEventListener;
